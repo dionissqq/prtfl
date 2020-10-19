927f1d4fc160444513895c2598964f118662edb5
gav(Denis_Kryvchuk) <dendenysyk@gmail.com>
2020-10-19 23:26:12 +0300

    woof

---
+++ unity mini golf game/Assets/Ball_Follower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Follower : MonoBehaviour
+{
+    Transform Balltr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Balltr = GameObject.FindGameObjectWithTag("Player").transform;
+        setCameraPos();
+    }
+    void setCameraPos(){
+        transform.position= new Vector3(Balltr.position.x,3,Balltr.position.z-2);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        setCameraPos();
+    }
+}
